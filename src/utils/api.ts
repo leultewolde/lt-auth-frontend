0c034206e3807646b2f7699272c4f32b7e2dd6e9
Leul Tewolde <leulwoldegabriel@gmail.com>
2025-01-02 11:14:27 -0600

    fixed env variable issue

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.API_URL || "http://localhost:8080";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080";
 
 export interface QueryParams {
     prefix: string;
